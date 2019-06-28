a47f8da3a08cb97e61280cecccdb2f4cc9065e48
Joe Ho <joebcrmd@gmail.com>
2019-06-28 23:02:09 +0800

    init test

---
+++ palindrome/test/index.test.js
@@ -0,0 +1,17 @@
+const index = require('../scripts');
+const test = require('tape');
+
+test('should return equal', function (t) {
+  t.equal(index.palindrome('a'), true);
+  t.end();
+})
+
+test('should return equal', function (t) {
+  t.equal(index.palindrome('Able was I ere I saw Elba'), true);
+  t.end();
+})
+
+test('should return not equal', function (t) {
+  t.equal(index.palindrome('string'), true);
+  t.end();
+})
